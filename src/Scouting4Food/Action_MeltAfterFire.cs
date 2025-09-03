309d4291fefe1050a2060ddcb02c63c9d9b14f88
CoddingCatPL <pozdrox.yt@gmail.com>
2025-09-03 14:24:55 +0200

    GARAG
    GARAGA

---
+++ src/Scouting4Food/Action_MeltAfterFire.cs
@@ -0,0 +1,35 @@
+﻿using Photon.Pun;
+using UnityEngine;
+using Zorro.Core;
+
+public class ItemCookingSimple : ItemComponent
+{
+    [SerializeField] private bool disableCooking;
+    public bool canBeCooked => !disableCooking;
+
+
+    public override void OnInstanceDataSet()
+    {
+
+    }
+
+    [PunRPC]
+    private void FinishCookingRPC()
+    {
+        if (photonView.AmController)
+        {
+            PhotonNetwork.Destroy(gameObject);
+        }
+    }
+
+    public void FinishCooking()
+    {
+        if (!photonView.AmController) return;
+        photonView.RPC("FinishCookingRPC", RpcTarget.All);
+        if ((bool)item.holderCharacter && (bool)item.holderCharacter.GetComponent<CharacterItems>() && (bool)item.holderCharacter.GetComponent<CharacterItems>().cookSfx)
+        {
+            item.holderCharacter.GetComponent<CharacterItems>().cookSfx.Play(transform.position);
+        }
+        Debug.Log("Item Cooked and Destroyed");
+    }
+}
