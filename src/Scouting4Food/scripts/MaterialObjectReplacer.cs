7481ea14bf90e26dd416294329a0c97a3c1bd346
BurningSulphur <119114567+BurningSulphur@users.noreply.github.com>
2025-09-12 23:44:04 +0100

    Working decal fix. issue with smoke particle

--- src/Scouting4Food/scripts/MaterialObjectReplacer.cs
+++ src/Scouting4Food/scripts/MaterialObjectReplacer.cs
@@ -10,6 +10,13 @@ public class MaterialObjectReplacer : MonoBehaviour
 
     [Tooltip("Name of the shader to apply to materials")]
     public string shaderName = "W/Peak_Standard";
+    
+    
+    [Tooltip("Name of the shader to apply to special materials")]
+    public string specialShaderName = "Decal";
+    
+    [Tooltip("The Material that gets special treatment")]
+    public Material SpecialMaterial;
 
     private void Start()
     {
@@ -23,10 +30,18 @@ public class MaterialObjectReplacer : MonoBehaviour
             Debug.LogWarning("MaterialObjectReplacer: Target object or shader name not set.");
             return;
         }
+        
         Shader newShader = Shader.Find(shaderName);
         if (newShader == null)
         {
             Debug.LogError($"MaterialObjectReplacer: Shader '{shaderName}' not found!");
+            return;
+        }
+        
+        Shader newSpecialShader = Shader.Find(specialShaderName);
+        if (SpecialMaterial != null && newSpecialShader == null)
+        {
+            Debug.LogError($"MaterialObjectReplacer: Shader '{specialShaderName}' not found!");
             return;
         }
 
@@ -38,26 +53,25 @@ public class MaterialObjectReplacer : MonoBehaviour
 
             for (int i = 0; i < renderer.sharedMaterials.Length; i++)
             {
-                if (renderer.sharedMaterials[i] != null)
+                Material originalMat = renderer.sharedMaterials[i];
+                if (originalMat != null)
                 {
-                    Material updatedMat = new Material(renderer.sharedMaterials[i]);
-                    updatedMat.shader = newShader;
+                    Material updatedMat = new Material(originalMat);
+
+                    if (SpecialMaterial != null && originalMat.name.Replace(" (Instance)", "") == SpecialMaterial.name)
+                    {
+                        updatedMat.shader = newSpecialShader;
+                        Debug.Log($"Applied SPECIAL shader '{specialShaderName}' to material '{originalMat.name}'");
+                    }
+                    else
+                    {
+                        updatedMat.shader = newShader;
+                    }
+
                     updatedMats[i] = updatedMat;
                 }
             }
-
             renderer.materials = updatedMats; // replaces *all* materials
-            
-            /*
-            if (renderer.sharedMaterial != null)
-            {
-                Material updatedMat = new Material(renderer.sharedMaterial);
-
-                updatedMat.shader = newShader;
-
-                renderer.material = updatedMat;
-            }
-            */
         }
     }
 }
